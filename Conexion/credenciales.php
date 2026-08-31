e1456d9dc78fcc1dcd392bcda1dcbfbacab5a9f2
Lisandro Müller <liso.mendoza1@gmail.com>
2026-08-31 18:26:17 -0300

    Update credenciales.php

--- Conexion/credenciales.php
+++ Conexion/credenciales.php
@@ -1,19 +1,1 @@
-<?php
-/**
- * Lizzosoft Vehículos - Credenciales Globales
- * IMPORTANTE: ¡Agregar este archivo al .gitignore!
- */
-return [
-    // --- Credenciales de Base de Datos ---
-    'host' => 'localhost',
-    'db' => 'c110lizzosoftVehiculos',
-    'user' => 'c110lizzosoftVehiculos',
-    'pass' => 'xU@7ZomPWk',
 
-    // --- Credenciales de Correo (SMTP) ---
-    'mail_host' => 'mail.infoar.net',
-    'mail_user' => 'info@lizzosoft.com.ar',
-    'mail_pass' => '2@g9HGXfyj',
-    'mail_port' => 465
-];
-?>
